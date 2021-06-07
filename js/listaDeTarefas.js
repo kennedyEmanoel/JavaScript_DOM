5bf2dde033c6cdddf4a5ab4bb02735f1017bb305
Kennedy Emanoel <kennedy1emanoel19@gmail.com>
2021-06-07 09:48:29 -0300

    jsDOM

---
+++ js/listaDeTarefas.js
@@ -0,0 +1,22 @@
+const novaTarefa = document.querySelector('[data-form-button]')
+const inputTarefa = document.querySelector('[data-form-input]')
+
+function criarTarefa(event){
+    event.preventDefault()
+
+    const valorTarefa = inputTarefa.value
+    const listaDeTarefas = document.querySelector('[data-task]')
+
+    novaLabel = document.createElement('label')
+    novaLabel.innerText = valorTarefa
+    novaLabel.className = "form-check-label"
+
+    novoItem = document.createElement('li')
+    novoItem.appendChild(novaLabel)
+
+    listaDeTarefas.appendChild(novoItem)
+    inputTarefa.value = ""
+
+}
+
+novaTarefa.addEventListener('click', criarTarefa)
